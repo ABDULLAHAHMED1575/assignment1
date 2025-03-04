6b269ceae7afefbf314f4da4d7f556b7882ce261
ABDULLAHAHMED1575 <abdullahmed1575@gmail.com>
2025-03-04 06:35:03 +0500

    Assignment1

---
+++ lib/main.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Assignment1',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
+      ),
+      home: const MyHomePage(title: 'Assignment 1'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+  double _fontSize = 16.0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter+=50;
+      if(_fontSize < 20.0){
+        _fontSize+=0.2;
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blue,
+        title: Text(
+          widget.title,
+          style: TextStyle(
+              fontSize: 50.0,
+              color: Colors.brown.shade100
+          ),
+        ),
+
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+                'You have pushed the button this many times:',
+                style: TextStyle(fontSize: _fontSize),
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        backgroundColor: Colors.deepPurpleAccent.shade100,
+        child: const Icon(Icons.add),
+      ),
+
+      // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
